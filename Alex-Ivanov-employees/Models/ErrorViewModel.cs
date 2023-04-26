0f78120e3636bc64f84fb299bfb50356cce82671
IvanovvAlex <alexivanovv4@gmail.com>
2023-04-26 16:41:51 +0300

    Project setup

---
+++ Alex-Ivanov-employees/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Alex_Ivanov_employees.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
